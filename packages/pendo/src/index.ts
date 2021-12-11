f2e334855fa5e44e49a2a8e313a6f343263b719f
Dane Thurber <dane.thurber@gmail.com>
2021-12-11 12:15:13 -0700

    feat(analytics-plugin-pendo): create initial implementation

--- packages/pendo/src/index.ts
+++ packages/pendo/src/index.ts
@@ -1,29 +1,77 @@
-import { AnalyticsInstance } from "analytics";
+import { AnalyticsInstance, AnalyticsPlugin } from "analytics";
+import load from "@segment/load-script";
 
-export interface IPluginProps {
-  instance: AnalyticsInstance;
-  config: IPluginConfig;
+export interface PendoPlugin extends AnalyticsPlugin {
+  config: PluginConfig;
+
+  initialize: (props: {
+    config: PluginConfig;
+    instance: AnalyticsInstance;
+  }) => any;
+
+  identify: (props: {
+    config: PluginConfig;
+    instance: AnalyticsInstance;
+    payload: {
+      userId: string;
+      traits?: {
+        account?: Record<string, any>;
+        [key: string]: any;
+      };
+    };
+  }) => any;
+
+  track?: (props: {
+    config: PluginConfig;
+    instance: AnalyticsInstance;
+    payload: { event: string; properties: any };
+  }) => any;
 }
 
-type IPluginConfig = {
-  appId: string;
-};
+export interface PluginConfig {
+  apiKey: string;
+}
 
-const defaults = {
-  enabled: true
-};
+const defaults: Partial<PluginConfig> = {};
 
-export default function plugin(config: IPluginConfig) {
+export default function plugin(config: PluginConfig): PendoPlugin {
   return {
-    name: "pendo-analytics",
+    name: "pendo",
     config: { ...defaults, ...config },
 
-    initialize: (_plugin: IPluginProps) => {},
+    initialize: ({ config }) => {
+      if (!config.apiKey) throw new Error("Pendo apiKey required");
 
-    loaded: () => {},
+      window.pendo_config = window.pendo_config || {
+        apiKey: config.apiKey
+      };
 
-    identify: () => {},
+      if (typeof window.pendo === "undefined") {
+        load(`https://cdn.pendo.io/agent/static/${config.apiKey}/pendo.js`);
+      }
 
-    track: () => {}
+      window.pendo.initialize();
+    },
+
+    loaded: () => !!window.pendo && window.pendo.isReady(),
+
+    identify: ({ instance, payload }) => {
+      if (typeof window.pendo === "undefined") return;
+
+      const { traits = {} } = payload;
+
+      const visitor = {
+        ...traits.visitor,
+        id: instance.user("userId")
+      };
+
+      window.pendo.identify({ ...traits, visitor });
+    },
+
+    track: ({ payload }) => {
+      if (typeof window.pendo === "undefined") return;
+
+      window.pendo.track(payload.event, payload.properties);
+    }
   };
 }
--- packages/pendo/test/index.test.ts
+++ packages/pendo/test/index.test.ts
@@ -1,5 +1,89 @@
-describe("test", () => {
-  it("works", () => {
-    expect(true).toBe(true);
+import Analytics, { AnalyticsInstance } from "analytics";
+import load from "@segment/load-script";
+
+import PendoPlugin from "../src";
+
+jest.mock("@segment/load-script");
+
+const sleep = (ms = 20) => new Promise(resolve => setTimeout(resolve, ms));
+
+describe("PendoPlugin", () => {
+  const loadMock = load as jest.Mock;
+
+  const apiKey = "mock-api-key";
+
+  const accountId = "mock-user-account-id";
+  const userId = "mock-user-id";
+
+  let analytics: AnalyticsInstance;
+  let plugin;
+
+  beforeEach(() => {
+    loadMock.mockReset().mockImplementationOnce(() => {
+      window.pendo = ({
+        identify: jest.fn(),
+        initialize: jest.fn(),
+        isReady: jest.fn().mockReturnValue(true),
+        track: jest.fn()
+      } as unknown) as typeof window.pendo;
+    });
+
+    plugin = PendoPlugin({ apiKey });
+
+    analytics = Analytics({
+      app: "test-instance",
+      plugins: [plugin]
+    });
+  });
+
+  describe("#initialize", () => {
+    let readyMock = jest.fn();
+
+    beforeEach(async () => {
+      readyMock.mockReset();
+      analytics.ready(readyMock);
+
+      analytics.page();
+      await sleep();
+    });
+
+    it("should load the pendo script", async () => {
+      expect(loadMock).toHaveBeenCalledWith(
+        `https://cdn.pendo.io/agent/static/${apiKey}/pendo.js`
+      );
+      expect(window.pendo).not.toBeUndefined();
+    });
+
+    it("should enter ready state", async () => {
+      expect(readyMock).toHaveBeenCalled();
+    });
+  });
+
+  describe("#identify", () => {
+    beforeEach(async () => {
+      analytics.identify(userId, { account: { id: accountId } });
+      await sleep();
+    });
+
+    it("should call the pendo identify event with parsed traits", () => {
+      expect(window.pendo.identify).toHaveBeenCalledWith({
+        account: { id: accountId },
+        visitor: { id: userId }
+      });
+    });
+  });
+
+  describe("#track", () => {
+    const eventName = "mock-track-event";
+    const eventPayload = { key: "value" };
+
+    beforeEach(async () => {
+      analytics.track(eventName, eventPayload);
+      await sleep();
+    });
+
+    it("should call the pendo track event", () => {
+      expect(window.pendo.track).toHaveBeenCalledWith(eventName, eventPayload);
+    });
   });
 });
